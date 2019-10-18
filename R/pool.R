f866e9c205a2e37470aed7889ca892ee03606db6
Hong Ooi <hongooi@microsoft.com>
2019-10-19 06:56:45 +1100

    pool exports

--- R/pool.R
+++ R/pool.R
@@ -2,7 +2,7 @@
 #'
 #' @param size The number of background R processes to create. Limit this is you are low on memory.
 #' @param restart For `init_pool`, whether to terminate an already running pool first.
-#' @param ... Other arguments passed on to `parallel::makeCluster`.
+#' @param ... Other arguments passed on to functions in the parallel package.
 #'
 #' @details
 #' AzureRMR provides the ability to parallelise communicating with Azure by utilizing a pool of R processes in the background. This often leads to major speedups in scenarios like downloading large numbers of small files, or communicating with a cluster of virtual machines. The pool is created by calling `init_pool`. It remains persistent for the session or until terminated by `delete_pool`.
@@ -16,11 +16,11 @@ init_pool <- function(size=10, restart=FALSE, ...)
     if(restart)
         delete_pool()
 
-    if(!exists("pool", envir=.AzureR) || length(.AzureR$pool) < size)
+    if(pool_exists() || length(.AzureR$pool) < size)
     {
         delete_pool()
         message("Creating background pool")
-        .AzureR$pool <- parallel::makeCluster(size)
+        .AzureR$pool <- parallel::makeCluster(size, ...)
         parallel::clusterEvalQ(.AzureR$pool, loadNamespace("AzureStor"))
     }
     else
@@ -50,6 +50,73 @@ delete_pool <- function()
 }
 
 
+#' @rdname pool
 #' @export
+pool_exists <- function()
+{
+    exists("pool", envir=.AzureR) && inherits(.AzureR$pool, "cluster")
+}
+
+
+#' @rdname pool
+#' @export
+pool_export <- function(...)
+{
+    pool_check()
+    parallel::clusterExport(cl=.AzureR$pool, ...)
+}
+
+
+#' @rdname pool
+#' @export
+pool_lapply <- function(...)
+{
+    pool_check()
+    parallel::parLapply(cl=.AzureR$pool, ...)
+}
+
+
+#' @rdname pool
+#' @export
+pool_sapply <- function(...)
+{
+    pool_check()
+    parallel::parSapply(cl=.AzureR$pool, ...)
+}
+
+
+#' @rdname pool
+#' @export
+pool_map <- function(...)
+{
+    pool_check()
+    parallel::clusterMap(cl=.AzureR$pool, ...)
+}
+
+
+#' @rdname pool
+#' @export
+pool_call <- function(...)
+{
+    pool_check()
+    parallel::clusterCall(cl=.AzureR$pool, ...)
+}
+
+
+#' @rdname pool
+#' @export
+pool_evalq <- function(...)
+{
+    pool_check()
+    parallel::clusterEvalQ(cl=.AzureR$pool, ...)
+}
+
+
 .AzureR <- new.env()
 
+
+pool_check <- function()
+{
+    if(!pool_exists())
+        stop("AzureR pool does not exist; call init_pool() to create it", call.=FALSE)
+}
